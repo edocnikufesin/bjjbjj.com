641abc5fe8047b86ad24053a5fa4b181364c0211
michael <michaelroyburn@gmail.com>
2023-03-08 17:18:58 -0500

    Restyle menus

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,19 @@
-function hideStuff(listID) {
-  var x = document.getElementById(listID);
+function toggleList(listId) {
+  var x = document.getElementById(listId);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
+}
+
+function swapIcon(iconId) {
+  var y = document.getElementById(iconId);
+  y.classList.toggle("fa-caret-up");
+  y.classList.toggle("fa-caret-down");
+}
+
+function hideStuff(x,y) {
+  toggleList(x);
+  swapIcon(y);
 }
